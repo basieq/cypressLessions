1a5f663213ccce9a5c6f190cdf28bc3a6022355a
basieq85 <zyla.barbara@gmail.com>
2022-06-23 10:09:00 +0200

    functions in js

--- functions.js
+++ functions.js
@@ -1,1 +1,26 @@
 //functions in js
+
+function first_one (){
+    return console.log("my first function")
+}
+
+first_one();
+
+
+// another ways of functions cannot be executed before it initialization
+
+const second_one = () => {
+    return console.log("My second function")
+}
+
+second_one();
+
+
+// next exercise 
+
+function add_num() {
+    const adding = 2+5
+    return console.log("another day another exercise " + adding)
+}
+
+add_num()
